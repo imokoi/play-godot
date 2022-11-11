9b2599ed2b189d1e4a4750f3bfbd27b591fbc162
imokoi <tiantengfei@outlook.com>
2022-11-11 11:58:47 +0900

    update

--- scenes/HUD.gd
+++ scenes/HUD.gd
@@ -3,28 +3,27 @@ extends CanvasLayer
 signal start_game
 
 func show_message(text):
-	$Message.text = text
-	$Message.show()
-	$MessageTimer.start()
+    $Message.text = text
+    $Message.show()
+    $MessageTimer.start()
 
 func show_game_over():
-	show_message("Game Over")
+    show_message("Game Over")
+    yield($MessageTimer, "timeout")
 
-	yield($MessageTimer, "timeout")
+    $Message.text = "Dodge the \nCreeps!"
+    $Message.show()
 
-	$Message.text = "Dodge the \nCreeps!"
-	$Message.show()
-
-	yield(get_tree().create_timer(1), "timeout")
-	$StartButton.show()
+    yield(get_tree().create_timer(1), "timeout")
+    $StartButton.show()
 
 func update_score(score):
-	$ScoreLabel.text = str(score)
+    $ScoreLabel.text = str(score)
 
 func _on_MessageTimer_timeout():
-	$Message.hide()
+    $Message.hide()
 
 
 func _on_StartButton_pressed():
-	$StartButton.hide()
-	emit_signal("start_game")
+    $StartButton.hide()
+    emit_signal("start_game")
--- scenes/Main.gd
+++ scenes/Main.gd
@@ -4,43 +4,45 @@ export(PackedScene) var mob_scene
 var score
 
 func _ready():
-	randomize()
+    randomize()
+    var console = JavaScript.get_interface("console")
+    console.log("Hello from GDScript!")
 
 func game_over():
-	$ScoreTimer.stop()
-	$MobTimer.stop()
-	$HUD.show_game_over()
+    $ScoreTimer.stop()
+    $MobTimer.stop()
+    $HUD.show_game_over()
 
 func new_game():
-	score = 0
-	$Player.start($StartPosition.position)
-	$StartTimer.start()
-	$HUD.update_score(score)
-	$HUD.show_message("Get Ready")
-	get_tree().call_group("mobs", "queue_free")
+    score = 0
+    $Player.start($StartPosition.position)
+    $StartTimer.start()
+    $HUD.update_score(score)
+    $HUD.show_message("Get Ready")
+    get_tree().call_group("mobs", "queue_free")
 
 func _on_ScoreTimer_timeout():
-	score += 1
-	$HUD.update_score(score)
+    score += 1
+    $HUD.update_score(score)
 
 func _on_StartTimer_timeout():
-	$MobTimer.start()
-	$ScoreTimer.start()
+    $MobTimer.start()
+    $ScoreTimer.start()
 
 
 func _on_MobTimer_timeout():
-	var mob = mob_scene.instance()
-	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
-	mob_spawn_location.offset = randi()
+    var mob = mob_scene.instance()
+    var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
+    mob_spawn_location.offset = randi()
 
 
-	var direction = mob_spawn_location.rotation + PI / 2
-	mob.position = mob_spawn_location.position
+    var direction = mob_spawn_location.rotation + PI / 2
+    mob.position = mob_spawn_location.position
 
-	direction += rand_range(-PI / 4, PI / 4)
-	mob.rotation = direction
+    direction += rand_range(-PI / 4, PI / 4)
+    mob.rotation = direction
 
-	var velocity = Vector2(rand_range(150.0, 250.0), 0.0)
-	mob.linear_velocity = velocity.rotated(direction)
+    var velocity = Vector2(rand_range(150.0, 250.0), 0.0)
+    mob.linear_velocity = velocity.rotated(direction)
 
-	add_child(mob)
+    add_child(mob)
